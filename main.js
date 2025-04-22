a839f8b1fff68ce3690bd7df6a33b5b6b2e89887
delgado-jason <delgadorjason7@gmail.com>
2025-04-22 16:07:44 -0500

    Initial setup

---
+++ main.js
@@ -0,0 +1,12 @@
+// Library that holds book objects
+const myLibrary = [];
+
+// The Book constructor
+function Book() {
+
+}
+
+// Add Book object to the library
+function addToLibrary() {
+    
+}
