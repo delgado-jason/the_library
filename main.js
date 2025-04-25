1eff6009b8a7af9ad2609f6fe6a30c135ed1c4c9
delgado-jason <delgadorjason7@gmail.com>
2025-04-24 23:29:50 -0600

    Fix issue with books not displaying on front-end in proper order.

--- main.js
+++ main.js
@@ -3,10 +3,7 @@ const section = document.getElementById('cards');
 const submitBtn = document.getElementById('submit-btn');
 
 // Library that holds book objects
-const myLibrary = [];
-
-// Fake Book Data
-const bookData = [
+const myLibrary = [
     {
       "id": 1,
       "title": "To Kill a Mockingbird",
@@ -43,78 +40,6 @@ const bookData = [
       "description": "A classic novel exploring themes of love, marriage, and social norms.",
       "cover_image": "https://fakeimg.pl/667x1000/cc6600"
     },
-    {
-      "id": 4,
-      "title": "The Great Gatsby",
-      "author": "F. Scott Fitzgerald",
-      "publication_year": 1925,
-      "genre": [
-        "Fiction",
-        "Classic"
-      ],
-      "description": "A tale of the American Dream, wealth, and love during the Roaring Twenties.",
-      "cover_image": "https://fakeimg.pl/667x1000/cc6600"
-    },
-    {
-      "id": 5,
-      "title": "Moby-Dick",
-      "author": "Herman Melville",
-      "publication_year": 1851,
-      "genre": [
-        "Fiction",
-        "Adventure"
-      ],
-      "description": "The epic tale of Captain Ahab's obsession with the white whale.",
-      "cover_image": "https://fakeimg.pl/667x1000/cc6600"
-    },
-    {
-      "id": 6,
-      "title": "The Lord of the Rings",
-      "author": "J.R.R. Tolkien",
-      "publication_year": 1954,
-      "genre": [
-        "Fantasy",
-        "Adventure"
-      ],
-      "description": "An epic fantasy saga about the quest to destroy the One Ring.",
-      "cover_image": "https://fakeimg.pl/667x1000/cc6600"
-    },
-    {
-      "id": 7,
-      "title": "The Catcher in the Rye",
-      "author": "J.D. Salinger",
-      "publication_year": 1951,
-      "genre": [
-        "Fiction",
-        "Coming-of-age"
-      ],
-      "description": "A classic coming-of-age novel following Holden Caulfield's journey.",
-      "cover_image": "https://fakeimg.pl/667x1000/cc6600"
-    },
-    {
-      "id": 8,
-      "title": "The Hobbit",
-      "author": "J.R.R. Tolkien",
-      "publication_year": 1937,
-      "genre": [
-        "Fantasy",
-        "Adventure"
-      ],
-      "description": "The prequel to The Lord of the Rings, following Bilbo Baggins' journey.",
-      "cover_image": "https://fakeimg.pl/667x1000/cc6600"
-    },
-    {
-      "id": 9,
-      "title": "One Hundred Years of Solitude",
-      "author": "Gabriel Garcia Marquez",
-      "publication_year": 1967,
-      "genre": [
-        "Magical Realism",
-        "Literary Fiction"
-      ],
-      "description": "A multi-generational saga of the Buendía family in the fictional town of Macondo.",
-      "cover_image": "https://fakeimg.pl/667x1000/cc6600"
-    }
   ]
 
 // The Book constructor
@@ -127,25 +52,6 @@ function Book(id, title, author, year, genre, desc, img) {
     this.desc = desc;
     this.img = img;
 }
-
-// Add Book object to the library
-function generateLibrary(id, title, author, year, genre, desc, img) {
-    const newBook = new Book(id, title, author, year, genre, desc, img);
-    myLibrary.push(newBook);
-}
-
-// Loop through book data and populate library
-bookData.forEach((book) => {
-    id = book.id;
-    title = book.title;
-    author = book.author;
-    year = book["publication_year"];
-    genre = book.genre[0];
-    desc = book.description;
-    image = book["cover_image"];
-
-    generateLibrary(id, title, author, year, genre, desc, image);
-})
 
 // Add new book to library
 function addBookToLibrary(title, author, year, genre, desc) {
@@ -177,14 +83,15 @@ function displaybooks() {
       `
       cardElem.className = 'card';
       section.appendChild(cardElem);
-
     })
-    
-    
-
 }
 
 displaybooks();
+
+// Resets the screen
+function resetScreen() {
+  section.innerHTML = '';
+}
 
 
 // Add event listener
@@ -200,21 +107,25 @@ submitBtn.addEventListener('click', (e) => {
   descInput = document.getElementById('desc').value;
 
   addBookToLibrary(titleInput, authorInput, yearInput, genreInput, descInput);
+
+  resetScreen();
+  myLibrary.sort((a, b) => b.id - a.id)
+  displaybooks();
   
   // Update display 
-  myLibrary.forEach((book) => {
-    const cardElem = document.createElement('div');
-      cardElem.innerHTML = `
-        <div id="${book.id}" class="book">
-          <h3 class="book-title">${book.title}</h3>
-          <p class="book-author">${book.author}</p>
-          <p class="book-genre">${book.genre}</p>
-          <p class="book-year">${book.year}</p>
-          <p class="book-desc">${book.desc}</p>
-        </div>
-      `
-      cardElem.className = 'card';
-      section.prepend(cardElem);
-  })
+  // myLibrary.forEach((book) => {
+  //   const cardElem = document.createElement('div');
+  //     cardElem.innerHTML = `
+  //       <div id="${book.id}" class="book">
+  //         <h3 class="book-title">${book.title}</h3>
+  //         <p class="book-author">${book.author}</p>
+  //         <p class="book-genre">${book.genre}</p>
+  //         <p class="book-year">${book.year}</p>
+  //         <p class="book-desc">${book.desc}</p>
+  //       </div>
+  //     `
+  //     cardElem.className = 'card';
+  //     section.prepend(cardElem);
+  //})
   
 })
